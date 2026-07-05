944c32da6a2656693f1b65c5492e3a2a5c057a49
karmic <karbemichael@gmail.com>
2026-07-05 16:16:58 +0200

    Add new swiftUIView

---
+++ SwiftFlix/SwiftFlix/HorizontalListView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SwiftFlix
+//
+//  Created by karmic on 7/5/26.
+//
+
+import SwiftUI
+
+struct HorizontalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalListView()
+}
