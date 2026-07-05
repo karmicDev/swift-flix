9d8ea3817ac0e3c82d634a9e05373c22df72e818
karmic <karbemichael@gmail.com>
2026-07-05 13:49:40 +0200

    Initial commit. Empty project.

---
+++ SwiftFlix/SwiftFlix/SwiftFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFlixApp.swift
+//  SwiftFlix
+//
+//  Created by karmic on 7/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
