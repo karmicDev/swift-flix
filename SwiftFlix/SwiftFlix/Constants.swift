a28d150b3464498a58eefee00c171968ec378081
karmic <karbemichael@gmail.com>
2026-07-05 14:14:51 +0200

    Add Constants file, with strings.

---
+++ SwiftFlix/SwiftFlix/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  SwiftFlix
+//
+//  Created by karmic on 7/5/26.
+//
+
+import Foundation
+
+struct Constants {
+  static let homeString = "Home"
+  static let upcomingString = "Upcoming"
+  static let searchString = "Search"
+  static let downloadString = "Download"
+}
