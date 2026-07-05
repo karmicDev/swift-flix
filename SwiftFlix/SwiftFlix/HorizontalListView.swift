ee2d11a5596560c94c21c78eb772a22db0e61264
karmic <karbemichael@gmail.com>
2026-07-05 16:31:37 +0200

    Add baic scrollView

--- SwiftFlix/SwiftFlix/HorizontalListView.swift
+++ SwiftFlix/SwiftFlix/HorizontalListView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct HorizontalListView: View {
+  let header = Constants.Strings.trendingMovies
+  var titles = [Constants.URLs.testTitle,
+                Constants.URLs.testTitle2,
+                Constants.URLs.testTitle3]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack(alignment: .leading) {
+        Text(header)
+          .font(.title)
+        ScrollView(.horizontal) {
+          LazyHStack {
+            ForEach(titles, id:\.self) { title in
+              AsyncImage(url: URL(string: title)) { image in
+                image
+                  .resizable()
+                  .scaledToFit()
+              } placeholder: {
+                ProgressView()
+              }
+              .frame(width: 120, height: 200)
+            }
+          }
+        }
+      }
+      .frame(height: 250)
+      .padding(10)
     }
 }
 
