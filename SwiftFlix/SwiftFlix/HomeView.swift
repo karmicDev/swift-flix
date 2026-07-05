ef93efc4739d2eee0a434fd57a649310d83f366b
karmic <karbemichael@gmail.com>
2026-07-05 16:52:00 +0200

    Add ListViews to homeView.

--- SwiftFlix/SwiftFlix/HomeView.swift
+++ SwiftFlix/SwiftFlix/HomeView.swift
@@ -9,27 +9,31 @@ import SwiftUI
 
 struct HomeView: View {
   var body: some View {
-    VStack {
-      AsyncImage(url: URL(string: Constants.URLs.testTitle)) { image in
-        image
-          .resizable()
-          .scaledToFit()
-      } placeholder: {
-        ProgressView()
-      }
-      HStack {
-        Button {
-          
-        } label: {
-          Text(Constants.Strings.play)
-            .ghostButton()
+    ScrollView {
+      LazyVStack {
+        AsyncImage(url: URL(string: Constants.URLs.testTitle)) { image in
+          image
+            .resizable()
+            .scaledToFit()
+        } placeholder: {
+          ProgressView()
         }
-        Button {
-          
-        } label: {
-          Text(Constants.Strings.download)
-            .ghostButton()
+        HStack {
+          Button {
+          } label: {
+            Text(Constants.Strings.play)
+              .ghostButton()
+          }
+          Button {
+          } label: {
+            Text(Constants.Strings.download)
+              .ghostButton()
+          }
         }
+        HorizontalListView(header: Constants.Strings.trendingMovies)
+        HorizontalListView(header: Constants.Strings.trendingTV)
+        HorizontalListView(header: Constants.Strings.topRatedMovies)
+        HorizontalListView(header: Constants.Strings.topRatedTV)
       }
     }
   }
