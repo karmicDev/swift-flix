a1420b109b4968cd7045badd297ddd147e96e702
karmic <karbemichael@gmail.com>
2026-07-09 19:56:13 +0200

    Add URLBilder struct

---
+++ SwiftFlix/SwiftFlix/Network/URLBuilder.swift
@@ -0,0 +1,58 @@
+//
+//  UrlBuilder.swift
+//  SwiftFlix
+//
+//  Created by karmic on 7/9/26.
+//
+
+import Foundation
+
+struct URLBuilder {
+  func url(for endpoint: Endpoint) throws -> URL {
+    guard let baseURL = tmdbBaseURL else {
+      throw NetworkError.missingConfig
+    }
+    guard let apiKey = tmdbApiKey else {
+      throw NetworkError.missingConfig
+    }
+
+    switch endpoint {
+    case .trending(let mediaType):
+      guard let titlesURL = URL(string: baseURL)?
+        .appending(path: "trending/\(mediaType.rawValue)/day")
+        .appending(queryItems: [
+          URLQueryItem(name: "api_key", value: apiKey)
+        ]) else {
+        throw NetworkError.urlBuildFailed
+      }
+      return titlesURL
+    case .movie(let id):
+      guard let movieURL = URL(string: baseURL)?
+        .appending(path: "movie/\(id)")
+        .appending(queryItems: [
+          URLQueryItem(name: "api_key", value: apiKey)
+        ]) else {
+        throw NetworkError.urlBuildFailed
+      }
+      return movieURL
+    case .tv(let id):
+      guard let movieURL = URL(string: baseURL)?
+        .appending(path: "tv/\(id)")
+        .appending(queryItems: [
+          URLQueryItem(name: "api_key", value: apiKey)
+        ]) else {
+        throw NetworkError.urlBuildFailed
+      }
+      return movieURL
+    case .search(let query):
+      guard let searchURL = URL(string: baseURL)?
+        .appending(path: "search/\(query)")
+        .appending(queryItems: [
+          URLQueryItem(name: "api_key", value: apiKey)
+        ]) else {
+        throw NetworkError.urlBuildFailed
+      }
+      return searchURL
+    }
+  }
+}
