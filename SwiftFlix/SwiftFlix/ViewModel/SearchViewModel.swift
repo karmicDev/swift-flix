25347c56d440a11a02098fbfe795244d611ff91d
karmic <karbemichael@gmail.com>
2026-07-27 19:03:34 +0200

    Add searchVIewModel

--- SwiftFlix/SwiftFlix/ViewModel/SearchViewModel.swift
+++ SwiftFlix/SwiftFlix/ViewModel/SearchViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+@Observable
+class SearchViewModel {
+  private(set) var errorMessage: String?
+  private(set) var searchTitles: [Title] = []
+  private let dataFetcher = DataFetcher()
+
+  func getSearchTitles(by media: String, for query: String) async {
+    do {
+      errorMessage = nil
+      if query.isEmpty {
+        searchTitles = try await dataFetcher.fetchTitle(for: .trendingMovie)
+      } else {
+        searchTitles = try await dataFetcher.fetchTitle(for: .search(query: query))
+      }
+    } catch {
+      print(error)
+      errorMessage = error.localizedDescription
+    }
+  }
+}
