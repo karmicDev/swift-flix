25fefe1421afe5ec857db78e8b82629271997701
karmic <karbemichael@gmail.com>
2026-07-27 17:09:59 +0200

    Add new file: UpcomingView

---
+++ SwiftFlix/SwiftFlix/Views/UpcomingView.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingView.swift
+//  SwiftFlix
+//
+//  Created by karmic on 7/27/26.
+//
+
+import SwiftUI
+
+struct UpcomingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpcomingView()
+}
